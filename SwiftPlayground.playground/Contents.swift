73b281695e3b9c8915574a6c705e76e9343962f9
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-04-24 13:51:21 -0400

    vars and lets

---
+++ SwiftPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+var age = 123
+
+age
+
+age = 35
+
+age
+
+let finalAge = 12
+
+var wallet = 13
+
+// Got paid
+wallet = 94
+
+// Got paid again
+wallet = 194
+
+print(wallet)
+
+var weather = "Sunny"
+
+let firstName = "matt"
+
+let height = 70
+
+// Here is my comment
+
+print("Hey my name is " + firstName)
