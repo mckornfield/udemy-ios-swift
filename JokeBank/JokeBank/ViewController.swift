222e754410d111aa9213f33aaee6f3f6d54a2617
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-04-26 10:25:27 -0400

    initial table view for jokes

---
+++ JokeBank/JokeBank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JokeBank
+//
+//  Created by Matthew Kornfield on 4/26/19.
+//  Copyright © 2019 Matthew Kornfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
