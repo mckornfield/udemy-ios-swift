6a58ffa03a90616ad46805e89bae00155bfdceba
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-04-30 09:40:20 -0400

    hello app done

---
+++ Collector/Collector/CreateCollectableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CreateCollectableViewController.swift
+//  Collector
+//
+//  Created by Matthew Kornfield on 4/30/19.
+//  Copyright © 2019 Matthew Kornfield. All rights reserved.
+//
+
+import UIKit
+
+class CreateCollectableViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func mediaTapped(_ sender: Any) {
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
